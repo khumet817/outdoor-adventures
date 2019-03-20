d3744722813fdacb6fc4d3c0a756ea4d802cbe58
khumet817 <tabilekhume17@gmail.com>
2019-03-20 15:09:38 +0200

    added js

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {slideDown()};
+
+function slideDown() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+}
